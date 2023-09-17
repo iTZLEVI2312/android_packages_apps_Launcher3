48aaab2fba634992d229c988f2ab0ca6466d4b7a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-17 10:45:57 +0800

    Launcher3: Hide freeform shortcut if app doesnt support freeform
    Change-Id: I06669608d77995c7d9f65f3b4a14872f67bea208
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -62,6 +62,7 @@ import com.android.launcher3.util.PackageUserKey;
 import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.widget.WidgetsBottomSheet;
 import com.android.launcher3.customization.InfoBottomSheet;
+import com.android.systemui.shared.system.ActivityManagerWrapper;
 
 import java.util.Arrays;
 import java.net.URISyntaxException;
@@ -529,7 +530,9 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
     }
 
     public static final Factory<BaseDraggingActivity> FREE_FORM = (activity, itemInfo, originalView) -> 
-        new FreeForm(activity, itemInfo, originalView);
+        ActivityManagerWrapper.getInstance().supportsFreeformMultiWindow(activity) 
+        ? new FreeForm(activity, itemInfo, originalView)
+        : null;
 
     public static class FreeForm extends SystemShortcut<BaseDraggingActivity> {
         private final String mPackageName;
